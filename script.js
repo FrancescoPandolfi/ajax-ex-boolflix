163f4237386bce334d75246cdf3ed1989fa10912
Francesco Pandolfi <francesco.pan@gmail.com>
2020-02-08 12:46:35 +0100

    INserita chiamata alle serie tv e i poster

--- script.js
+++ script.js
@@ -22,27 +22,49 @@ function ajaxRequest(query) {
   $.ajax({
   url: "https://api.themoviedb.org/3/search/movie",
   method: "GET",
+  total_results: 10,
   data: {
     api_key: 'b7db4886e799d733a0c24ab663a6b884',
     query: query
   },
   success: function (data, stato) {
-    var results = data.results;
+    var movies = data.results;
+    secondAjaxReq(movies, query);
+  },
+  error: function (richiesta, stato, errore) {
+    $('.movies').append("<li>È avvenuto un errore. " + errore + "</li>");
+  }
+  });
+}
+
+function secondAjaxReq(movies, query) {
+  $.ajax({
+  url: "https://api.themoviedb.org/3/search/tv",
+  method: "GET",
+  data: {
+    api_key: 'b7db4886e799d733a0c24ab663a6b884',
+    query: query
+  },
+  success: function (data, stato) {
+    var series = data.results;
+     var results = movies.concat(series);
+     console.log(results);
     roundTheVote(results);
     printResult(results, query);
   },
   error: function (richiesta, stato, errore) {
-    alert("È avvenuto un errore. " + errore);
+    $('.movies').append("<li>È avvenuto un errore. " + errore + "</li>");
   }
   });
 }
+
 
 
 function printResult(results, query) {
 
   resetContent();
 
-  // handlebars select source and compile template
+  // select source and select template for handlebars
   var source = $("#movies-template").html();
   var template = Handlebars.compile(source);
 
@@ -53,31 +75,32 @@ function printResult(results, query) {
     $('.movies').append(`<li>Your search for ${query} did not have any matches.</li>`);
   }
 
-  // handlebars generate and insert content
+  // generate content for handlebars
   results.forEach(function(item) {
 
+    let title;
+    if (!item.title) {
+      title = item.name;
+    } else {
+      title = item.title;
+    }
+
     var context = {
-      title: item.title,
-      lang: function setFlag() {
-        return item.original_language;
-      },
-      vote: item.vote_average,
+      title: title,
+      lang: setFlag(item),
+    poster_path: 'https://image.tmdb.org/t/p/w92/' + item.poster_path
+      // vote: item.vote_average,
     };
     addStars(item, context);
 
+    // {handlebars} compile template with all the data
     var html = template(context);
     $(".movies").append(html);
-
-
-    // $(".flags").onerror = function() {
-    //   $(".lang").html("item.original_language");
-    // };
-
 
   });
 }
 
-  // Svuota tutto
+  // remove all
 function resetContent() {
   $(".movies").html('');
 }
@@ -85,7 +108,7 @@ function resetContent() {
 // Ordina in base alla popolarità
 function orderResults(results) {
   results.sort(function(b, a){
-    return a.vote_average - b.vote_average;
+    return a.popularity - b.popularity;
   });
 }
 
@@ -98,7 +121,6 @@ function roundTheVote(results) {
 
 // Inserisce le stelle piene in base al numero della votazione
 function addStars(item, context) {
-
   for (var i = 1; i <= 5; i++) {
     if (item.vote_average < i) {
       context["star" + i] = "far";
@@ -106,5 +128,14 @@ function addStars(item, context) {
       context["star" + i] = "fas";
     }
   }
+}
 
+function setFlag(item) {
+  var availableFlags = ['en', 'de', 'es', 'fr', 'it', 'pt'];
+  var itemLanguage = item.original_language;
+  if (availableFlags.includes(itemLanguage)) {
+    return `img/lang/${itemLanguage}.svg`;
+  } else {
+    $('.lang').html(itemLanguage);
+  }
 }
